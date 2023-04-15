7c14bb098ec99d014dc788e8160ab4a0496c8f3b
Andrew <designwebmoldova@gmail.com>
2023-04-15 13:38:32 +0300

    Changed property to public from private.

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,7 +2,7 @@
 
 require_once 'config.php';
 class Database {
-    private $connection;
+    public $connection;
     public function open_db_connection() {
         $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
         if(mysqli_connect_errno()) {
