8128c6565685b85ed1d8c8815c332f8da0cc21cc
Andrew <designwebmoldova@gmail.com>
2023-04-15 12:34:13 +0300

    Added code for DB connections.

---
+++ admin/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+const DB_HOST = 'localhost:3306';
+const DB_USER = 'root';
+const DB_PASS = 'root';
+const DB_NAME = 'gallery_db';
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+//$connection = mysqli_connect() or die('Error : ' . mysql_error());
+
+//$connection::mysql_error();
+
+if ($connection) {echo "true";}
