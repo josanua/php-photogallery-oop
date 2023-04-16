4ea6c7890b102862fb0bbe0d8979bfa94fed33ac
Andrew <designwebmoldova@gmail.com>
2023-04-16 09:02:50 +0300

    Try to do some tests with db in admin_content file

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,3 +1,7 @@
+<?php
+ include 'init.php';
+?>
+
 <div class="container-fluid">
 
     <!-- Page Heading -->
@@ -7,6 +11,17 @@
                 Admin
                 <small>Subheading</small>
             </h1>
+
+            <?php
+                $db = new Database();
+                $sql = "SELECT * FROM  users WHERE id=1";
+                $result = $db->query($sql);
+                $user_found = mysqli_fetch_array($result);
+
+//                echo $user_found['user_name'];
+//                var_dump($result);
+            ?>
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
