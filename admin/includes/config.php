548a51be5527ce4bd754d54e60ca0c5f5304d2ed
Andrew <designwebmoldova@gmail.com>
2023-04-17 09:58:39 +0300

    Some cleaning of config file

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,8 +6,5 @@ const DB_PASS = 'root';
 const DB_NAME = 'gallery_db';
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-//$connection = mysqli_connect() or die('Error : ' . mysql_error());
-
-//$connection::mysql_error();
 
 if ($connection) {echo "DB Connection true";}
