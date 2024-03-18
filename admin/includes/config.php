fc22ef9e15a7854ea4f9d81cb9c5f83221a6dd87
Josanu Andrei <andrei@quadra.md>
2024-03-18 20:05:53 +0200

    Implement small necessary changes

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
-const DB_HOST = 'localhost:3306';
+const DB_HOST = 'localhost:8889';
 const DB_USER = 'root';
 const DB_PASS = 'root';
-const DB_NAME = 'gallery_db';
+const DB_NAME = 'cms_photogallery_oop';
 
 //$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
