18a9ee1acf792c78464f537b385d1acc5da0e20f
Josanu Andrei <andrei@quadra.md>
2024-04-23 09:55:23 +0300

    Add find_this_query method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -26,6 +26,19 @@ class User {
         return $result_set;
     }
 
+    public static function find_this_query($sql) {
+        global $database;
+
+        $result_set = $database->query($sql);
+        $the_object_array = array();
+
+        while ($row = mysqli_fetch_array($result_set)) {
+            $the_object_array[] = self::instantiation($row);
+        }
+
+        return $the_object_array;
+    }
+
     public static function instantiation($the_record) {
 
         $the_object = new self;
