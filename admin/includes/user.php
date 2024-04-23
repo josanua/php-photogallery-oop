4c5f96b34ab1a561bb69bde6650a530e337189e3
Josanu Andrei <andrei@quadra.md>
2024-04-23 09:07:50 +0300

    Create has_the_attribute method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -44,4 +44,10 @@ class User {
 
         return $the_object;
     }
+
+    private function has_the_attribute($the_attribute) {
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($the_attribute, $object_properties);
+    }
 }
