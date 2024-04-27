09dfbc8cfb00acd97bd98a70e29e214136d0d1c5
Josanu Andrei <andrei@quadra.md>
2024-04-27 18:40:23 +0300

    Make Session class

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,15 +1,30 @@
 <?php
 
-class Session {
-    private $signed_in;
+class Session
+{
+    private $signed_in = false;
     public $user_id;
 
-    function __construct() {
+    function __construct()
+    {
         session_start();
+        $this->check_the_login();
     }
 
-    private function check_the_login() {
-        if(isset($_SESSION['user_id'])) {
+    public function is_signed_in()
+    {
+        return $this->signed_in;
+    }
+
+    public function login($user)
+    {
+        $this->user_id = $_SESSION['user_id'] = $user->id;
+        $this->signed_in = true;
+    }
+
+    private function check_the_login()
+    {
+        if (isset($_SESSION['user_id'])) {
             $this->user_id = $_SESSION['user_id'];
             $this->signed_in = true;
         } else {
